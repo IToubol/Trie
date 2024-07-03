c268e212082dfe767370dcc36d2872a6fa82a022
Israel <israeltoubol@gmail.com>
2024-07-03 02:37:49 +0200

    corrections

--- trie.py
+++ trie.py
@@ -10,7 +10,7 @@ class _Node:
         self.extensions[key[0]] = next_node
         if len(key) > 1:
             next_node.extend(key[1:])
-    
+
     def contains(self, key:str) -> bool:
         if key[0] == self.value:
             if len(key) > 1:
@@ -21,51 +21,57 @@ class _Node:
         return False
     
     def is_leaf(self) -> bool:
-        return bool(self.extensions)
+        return not bool(self.extensions)
     
     def leaf(self, key:str) -> _Node:
-        if self.is_leaf() or not key:
+        if len(key) == 1:
             return self
-        return self.extensions[key[0]].leaf(key[1:])
+        return self.extensions[key[1]].leaf(key[1:])
 
     def contained_part_split(self, key:str, index:int=1) -> tuple[str, str]:
         if not self.contains(key[:index]):
             return key[:index-1], key[index-1:]
         return self.contained_part_split(key, index + 1)
-    
-    def __str__(self) -> str:
-        return f"|__{self.value }\n"
 
 
 class Trie:
     def __init__(self) -> None:
-        self.head = _Node("")
+        self.head = {"": _Node("")}
 
-    def contains(self, key:str) -> bool: 
-        return self.head.contains(key)
+    def contains(self, key:str) -> bool:
+        if key[0] in self.head:
+            return self.head[key[0]].contains(key)
+        return False
 
     def add(self, key:str) -> None:
-        if not self.head.contains(key):
-            contained_part, new_part = self.head.contained_part_split(key)
-            self.head.leaf(contained_part).extend(new_part)
+        if not self.contains(key):
+            if key[0] in self.head:
+                contained_part, new_part = self.head[key[0]].contained_part_split(key)
+                self.head[key[0]].leaf(contained_part).extend(new_part)
+            else:
+                node = _Node(key[0])
+                self.head[key[0]] = node
+                if len(key) > 1:
+                    node.extend(key[1:])
+            
 
-    def display(self) -> None:
-        if self.head.is_leaf():
-            print(self.head.value + "\n")
-        else:
-            for node in self.extensions:
-                self.display(node)
-
-
-
-
+# ==================================================== #
+#                       TESTS                          #
+# ==================================================== #
 if __name__ == "__main__":
     trie = Trie()
 
-    for key in ["à", "arbre", "art", "artiste", "chape", "chapeau", "créatif", "création", "œuf", "zèbre"]:
+    to_add_keys_list = [
+        "à", "arbre", "art", "artiste",
+        "chape", "chapeau", "créatif",
+        "création", "œuf", "zèbre"
+    ]
+    for key in to_add_keys_list:
         trie.add(key)
+
+    others_keys = ["Bonjour", "Aurevoir", "Pomme"]
     
-    print(f"L'arbre trie contient le mot 'arbre': {trie.contains("arbre")}")
-    print(f"L'arbre trie contient le mot 'bonjour': {trie.contains("bonjour")}")
-    
-    trie.display()
+    for key in others_keys:
+        print(f"L'arbre trie contient le mot {key}: {trie.contains(key)}")
+    for key in to_add_keys_list:
+        print(f"L'arbre trie contient le mot {key}: {trie.contains(key)}")
