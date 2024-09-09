74beee25cc0967446c51214b4c305ceb6fc331e3
Israel <israeltoubol@gmail.com>
2024-09-09 20:05:27 +0200

    correction de l'affichage

--- trie.py
+++ trie.py
@@ -1,3 +1,6 @@
+from typing import TypeVar
+
+TNode = TypeVar("TNode", bound="_Node")
 
 class _Node:
     def __init__(self, value:str, is_leaf:bool = False) -> None:
@@ -10,7 +13,8 @@ class Trie:
     def __init__(self) -> None:
         self.head = _Node("")
 
-    def _found_in(node:_Node, key:str) -> bool:
+    @staticmethod
+    def _found_in(node:TNode, key:str) -> bool:
         if key[0] in node.branches:
             if len(key) > 1:
                 return Trie._found_in(node.branches[key[0]], key[1:])
@@ -19,18 +23,21 @@ class Trie:
 
     def contains(self, key:str) -> bool:
         return Trie._found_in(self.head, key)
-    
-    def _contained_part_split(node:_Node, key:str, index:int=1) -> tuple[str, str]:
+
+    @staticmethod    
+    def _contained_part_split(node:TNode, key:str, index:int=1) -> tuple[str, str]:
         if not Trie._found_in(node, key[:index]):
             return key[:index-1], key[index-1:]
         return Trie._contained_part_split(node, key, index + 1)
-    
-    def _add_key_to(node:_Node, key:str) -> None:
+
+    @staticmethod
+    def _add_key_to(node:TNode, key:str) -> None:
         node.branches[key[0]] = _Node(key[0], len(key)==1)
         if len(key) > 1:
             Trie._add_key_to(node.branches[key[0]], key[1:])
-    
-    def _end_of_branch(node:_Node, branch:str) -> _Node:
+
+    @staticmethod
+    def _end_of_branch(node:TNode, branch:str) -> TNode:
         if not branch:
             return node
         if len(branch) == 1:
@@ -41,12 +48,26 @@ class Trie:
         contained, new_part = Trie._contained_part_split(self.head, key)
         if key != contained:
             return Trie._add_key_to(Trie._end_of_branch(self.head, contained), new_part)
-    
-    def _displaying(node:_Node, genealogy="", remaining_brothers_nb=0, word="") -> str:
-        next_genealogy = f"{genealogy}{"|  " if remaining_brothers_nb else "   "}"
-        leaf_part  = f" *[{word}{node.value}]\n{next_genealogy + ("|" if node.branches else "")}" if node.is_leaf else ""
+
+    @staticmethod
+    def _displaying(node:TNode, genealogy="", remaining_brothers_nb=0, word="") -> str:
+        """
+        recursive function to display the entire content of the tree
+
+        args
+            node               (TNode) : a _Node object representing the current character of the tree.
+            genealogy            (str) : a string representing the `branchs` of the next characters
+            remainig_brothers_nb (int) : the number of branchs from the same node.
+            word                 (str) : the retrived part of the current word
+        
+        return
+            str : the entire string of characters representing the tree.
+        """
+        next_genealogy = f"{genealogy}{"|  " if remaining_brothers_nb else "   " if node.value else ""}"
+        leaf_part  = f" *[{word}{node.value}]" if node.is_leaf else ""
         sons_nb = len(node.branches)-1
-        return f"{genealogy}|__{node.value}{leaf_part}\n{"".join(Trie._displaying(node.branches[key], next_genealogy, sons_nb-n, word+node.value) for n, key in enumerate(node.branches))}"
+
+        return f"{genealogy}|{"__" if node.value else ""}{node.value}{leaf_part}\n{"".join(Trie._displaying(node.branches[key], next_genealogy, sons_nb-n, word+node.value) for n, key in enumerate(node.branches))}"
 
     def __str__(self) -> str:
         return Trie._displaying(self.head)
@@ -62,8 +83,6 @@ if __name__ == "__main__":
     # print(f"{id(t1.add) = }\n{id(t1._end_of_branch) = }\n{id(t1.head) = }\n")
     # print(f"{id(t2.add) = }\n{id(t2._end_of_branch) = }\n{id(t2.head) = }")
 
-    trie = Trie()
-
     to_add_keys_list = [
         "à",
         "arbre", "arbuste", "arbustes",
@@ -72,10 +91,10 @@ if __name__ == "__main__":
         "chaud", "chaude", "chauds", "chaudes", "chaudement",
         "créatif", "création", "créance", "créancier",
         "œuf",
-        "zèbre"
+        "zèbre", "carotte"
     ]
 
+    trie = Trie()
     for key in to_add_keys_list:
         trie.add(key)
-
     print(trie)
